e929971dcc6bb1a3ebbed91b5a82964faede71f0
Verysatisfied <lmuiiu@foxmail.com>
2023-10-14 19:30:58 +0100

    change data

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
-import aboutSvg from "../assets/about.svg";
+import aboutSvg from "../assets/aboutMe.jpg";
 import SectionTitle from "./SectionTitle";
 const About = () => {
   return (
     <section className="bg-white py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
-        <img src={aboutSvg} className="w-full h-64" />
+        <img src={aboutSvg} className="w-full" />
+
         <article>
           <SectionTitle text="code and ME" />
           <p className="text-slate-600 mt-8 leading-loose">
